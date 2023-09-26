a13f1a53582497c5a1b1aefc26c863146a84a245
salman <salmankhan.faze@gmail.com>
2023-09-26 10:05:37 +0530

    expense first

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+    <>
+    <h1>Expense Item</h1>
+    <ul>
+        <li>food 10</li>
+        <li>petrol 100</li>
+        <li>movie 200</li>
+    </ul>
+    </>
+    );
+}
+export default ExpenseItem
