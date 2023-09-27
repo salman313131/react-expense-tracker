051345d0c85eaa5c76f58a9882a465b4548e879a
salman <salmankhan.faze@gmail.com>
2023-09-27 17:41:37 +0530

    useState

--- src/components/Expenses/ExpenseDetail.js
+++ src/components/Expenses/ExpenseDetail.js
@@ -1,9 +1,14 @@
-
+import { useState } from "react"
 function ExpenseDetail(props){
+    const [amount,setAmount] = useState(32)
+    const amountHandler=()=>{
+        setAmount(100)
+    }
     return(
     <div className='expense-item__description'>
             <h2>Car Insaurance {props.locationOfExpenditure}</h2>
-            <div className='expense-item__price'>$32</div>
+            <div className='expense-item__price'>${amount}</div>
+            <button onClick={amountHandler}>Change Value</button>
     </div>)
 }
 export default ExpenseDetail
