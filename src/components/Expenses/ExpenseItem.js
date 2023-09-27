5dc474f1109fb5bd2f1cef41c221c9075165edb8
salman <salmankhan.faze@gmail.com>
2023-09-27 14:44:44 +0530

    Event Listeners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,11 +2,14 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
 function ExpenseItem(props){
-   
+   const deletehandler = ()=>{
+    
+   }
     return (
     <div className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <ExpenseDetail locationOfExpenditure={props.locationOfExpenditure}></ExpenseDetail>
+        <button onClick={deletehandler}>Delete</button>
     </div>
     );
 }
