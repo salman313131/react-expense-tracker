a670accd66953620f4955b82a807209e06baf768
salman <salmankhan.faze@gmail.com>
2023-09-28 13:12:50 +0530

    Adding a filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import ExpenseItem from './components/Expenses/ExpenseItem'
 import NewExpense from './components/NewExpense/NewExpense'
+import ExpenseFilter from './components/Expenses/ExpenseFilter'
+import { useState } from 'react'
 function App() {
   const locationOfExpenditure = 'Mumbai'
   const expenseitemList=[]
@@ -9,9 +11,14 @@ function App() {
   const addExpenseHandler=(expense)=>{
     console.log(expense)
   }
+  const [filteredYear,setFilteredYear] = useState('2020')
+    const filterChangeHandler=(selectedYear)=>{
+        setFilteredYear(selectedYear)
+    }
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpenseFilter>
       {expenseitemList}
     </div>
   );
