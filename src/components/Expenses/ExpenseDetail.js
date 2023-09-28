6544d82542eda59525aa105fdf7eb7ffbf2d35c0
salman <salmankhan.faze@gmail.com>
2023-09-28 13:19:40 +0530

    Rendering Expenses with performance optimisations

--- src/components/Expenses/ExpenseDetail.js
+++ src/components/Expenses/ExpenseDetail.js
@@ -1,14 +1,8 @@
-import { useState } from "react"
 function ExpenseDetail(props){
-    const [amount,setAmount] = useState(32)
-    const amountHandler=()=>{
-        setAmount(100)
-    }
     return(
     <div className='expense-item__description'>
             <h2>Car Insaurance {props.locationOfExpenditure}</h2>
-            <div className='expense-item__price'>${amount}</div>
-            <button onClick={amountHandler}>Change Value</button>
+            <div className='expense-item__price'>$32</div>
     </div>)
 }
 export default ExpenseDetail
