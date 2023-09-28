8f1a7c4d816e799ffe16296011d03c767ddd3c27
salman <salmankhan.faze@gmail.com>
2023-09-28 08:21:56 +0530

    state inside the new Component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,6 +1,19 @@
 import './ExpenseForm.css'
+import { useState } from 'react'
 const ExpenseForm=()=>{
-    const titleHandler=(e)=>{
+    const [enteredTitle,updateEnteredTitle]=useState('')
+    const [enteredAmount,updateEnteredAmount]=useState('')
+    const [enteredDate,updateEnteredDate]=useState('')
+    const titleChangeHandler=(e)=>{
+        updateEnteredTitle(e.target.value)
+        console.log(e.target.value)
+    }
+    const amountChangeHandler=(e)=>{
+        updateEnteredAmount(e.target.value)
+        console.log(e.target.value)
+    }
+    const dateChangeHandler=(e)=>{
+        updateEnteredDate(e.target.value)
         console.log(e.target.value)
     }
     return(
@@ -8,15 +21,15 @@ const ExpenseForm=()=>{
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleHandler}></input>
+                    <input type='text' onChange={titleChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={titleHandler}></input>
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2023-12-31' onChange={titleHandler}></input>
+                    <input type='date' min='2019-01-01' max='2023-12-31' onChange={dateChangeHandler}></input>
                 </div>
                 <div className='new-expense__actions'>
                     <button type='submit'>Add Expense</button>
